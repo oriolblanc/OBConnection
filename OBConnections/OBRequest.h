3c1c432aaf13f07d89b1fa8d8f92522061c4eae5
Oriol Blanc <oriolblanc@gmail.com>
2012-09-09 09:21:25 +0200

    working on requests

--- OBConnections/OBRequest.h
+++ OBConnections/OBRequest.h
@@ -6,8 +6,32 @@
 //  Copyright (c) 2012 Oriol Blanc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+typedef enum {
+    OBRequestMethodTypeMethodGET       = 100,
+    OBRequestMethodTypeMethodPOST      = 200,
+    OBRequestMethodTypeMultiForm       = 300,
+} OBRequestMethodType;
+
+@class OBRequestParameters;
 
 @interface OBRequest : NSObject
+
+@property (nonatomic, assign) BOOL isPublic;
+@property (nonatomic, assign) BOOL retryLaterOnFailure;
+
++ (id)requestWithType:(OBRequestMethodType)_method
+             resource:(NSString *)_resource
+           parameters:(OBRequestParameters *)_parameters;
+
++ (id)requestWithType:(OBRequestMethodType)_method
+             resource:(NSString *)_resource
+           parameters:(OBRequestParameters *)_parameters
+             isPublic:(BOOL)isPublic;
+
++ (id)requestWithType:(OBRequestMethodType)_type
+             resource:(NSString *)_resource
+           parameters:(OBRequestParameters *)_parameters
+                files:(NSDictionary *)_files
+             isPublic:(BOOL)_isPublic;
 
 @end
--- OBConnections/OBRequest.m
+++ OBConnections/OBRequest.m
@@ -8,6 +8,172 @@
 
 #import "OBRequest.h"
 
+@interface OBRequest ()
+    @property (nonatomic, assign) OBRequestMethodType requestType;
+    @property (nonatomic, retain) NSString *resource;
+    @property (nonatomic, retain) OBRequestParameters *parameters;
+    @property (nonatomic, retain) NSDictionary *files;
+    @property (nonatomic, retain) NSMutableDictionary *requestHeaderFields;
+
+    + (id)requestWithIsPublic:(BOOL)_isPublic;
+@end
+
 @implementation OBRequest
+@synthesize isPublic = _isPublic;
+@synthesize retryLaterOnFailure = _retryLaterOnFailure;
+@synthesize requestType = _requestType;
+@synthesize resource = _resource;
+@synthesize parameters = _parameters;
+@synthesize files = _files;
+@synthesize requestHeaderFields = _requestHeaderFields;
+
+#pragma mark - Static constrictors
+
++ (id)requestWithIsPublic:(BOOL)_isPublic
+{
+    return [[[self alloc] initWithIsPublic:_isPublic] autorelease];
+}
+
++ (id)requestWithType:(OBRequestMethodType)_method
+             resource:(NSString *)_resource
+           parameters:(OBRequestParameters *)_parameters
+{
+    return [[[self alloc] initWithType:_method resource:_resource parameters:_parameters] autorelease];
+}
+
++ (id)requestWithType:(OBRequestMethodType)_method
+             resource:(NSString *)_resource
+           parameters:(OBRequestParameters *)_parameters
+             isPublic:(BOOL)_isPublic
+{
+    return [[[self alloc] initWithType:_method resource:_resource parameters:_parameters isPublic:_isPublic] autorelease];
+}
+
++ (id)requestWithType:(OBRequestMethodType)_method
+             resource:(NSString *)_resource
+           parameters:(OBRequestParameters *)_parameters
+                files:(NSDictionary *)_files
+             isPublic:(BOOL)_isPublic
+{
+    return [[[self alloc] initWithType:_method resource:_resource parameters:_parameters files:_files isPublic:_isPublic] autorelease];
+}
+
+#pragma mark - Instance constrictors
+
+- (id)initWithType:(OBRequestMethodType)type
+          resource:(NSString *)resource
+        parameters:(OBRequestParameters *)parameters
+{
+    return [self initWithType:type resource:resource parameters:parameters files:nil isPublic:NO];
+}
+
+- (id)initWithType:(OBRequestMethodType)type
+          resource:(NSString *)resource
+        parameters:(OBRequestParameters *)parameters
+          isPublic:(BOOL)isPublic
+{
+    if ((self = [self initWithIsPublic:isPublic])) {
+        self.requestType = type;
+        self.parameters = parameters;
+        self.files = nil;
+        
+        [self buildURL:resource];
+    }
+    
+    return self;
+}
+
+- (id)initWithType:(OBRequestMethodType)type
+          resource:(NSString *)resource
+        parameters:(OBRequestParameters *)parameters
+             files:(NSDictionary *)files
+          isPublic:(BOOL)isPublic
+{
+    if ((self = [self initWithIsPublic:isPublic]) ) {
+        self.requestType = type;
+        self.parameters = parameters;
+        self.files = files;
+        
+        [self buildURL:_resource];
+    }
+    
+    return self;
+}
+
+- (id)initWithIsPublic:(BOOL)isPublic
+{
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    
+    self.isPublic = isPublic;
+    self.retryLaterOnFailure = NO;
+    self.requestHeaderFields = [NSMutableDictionary dictionary];
+    
+    return self;
+}
+
+- (void)buildURL:(NSString *)resource
+{
+    NSString *buildUrl = nil;
+    
+    if (!self.isPublic)
+    {
+        buildUrl = [NSString stringWithFormat:@"secured/%@", resource];
+    }
+    else
+    {
+        buildUrl = resource;
+    }
+    
+    self.resource = [buildUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+}
+
+#pragma mark - NSCoding methods
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if ((self = [super init]))
+    {
+        self.requestType = [aDecoder decodeIntForKey:@"requestType"];
+        self.resource = [aDecoder decodeObjectForKey:@"resource"];
+        self.parameters = [aDecoder decodeObjectForKey:@"parameters"];
+        self.files = [aDecoder decodeObjectForKey:@"files"];
+        self.isPublic = [aDecoder decodeBoolForKey:@"isPublic"];
+        self.retryLaterOnFailure = [aDecoder decodeBoolForKey:@"retryLaterOnFailure"];
+    }
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeInt:self.requestType forKey:@"requestType"];
+    [aCoder encodeObject:self.resource forKey:@"resource"];
+    [aCoder encodeObject:self.parameters forKey:@"parameters"];
+    [aCoder encodeObject:self.files forKey:@"files"];
+    [aCoder encodeBool:self.isPublic forKey:@"isPublic"];
+    [aCoder encodeBool:self.retryLaterOnFailure forKey:@"retryLaterOnFailure"];
+}
+
+#pragma mark -
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"[OBRequest] URL: %@  parameters: %@", self.resource, self.parameters];
+}
+
+#pragma mark - Memory Management
+
+- (void)dealloc
+{
+    [_parameters release];
+    [_files release];
+    [_requestHeaderFields release];
+    [_resource release];
+    
+    [super dealloc];
+}
 
 @end
