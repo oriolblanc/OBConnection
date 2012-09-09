db799bda5434409f0a41d3db58bb6c38cd6bc122
Oriol Blanc <oriolblanc@gmail.com>
2012-09-09 08:59:39 +0200

    Connection object

---
+++ OBConnections/OBConnection.h
@@ -0,0 +1,13 @@
+//
+//  OBConnection.h
+//  OBConnections
+//
+//  Created by Oriol Blanc on 20/04/11.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OBConnection : NSObject
+
+@end
---
+++ OBConnections/OBConnection.m
@@ -0,0 +1,13 @@
+//
+//  OBConnection.m
+//  OBConnections
+//
+//  Created by Oriol Blanc on 20/04/11.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "OBConnection.h"
+
+@implementation OBConnection
+
+@end
