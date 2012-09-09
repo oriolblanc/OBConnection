8c79092fb1eae29ac7107f048c66c62520362605
Oriol Blanc <oriolblanc@gmail.com>
2012-09-09 08:41:21 +0200

    Create an iOS example project

---
+++ iOSExample/iOSExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSExample
+//
+//  Created by Oriol Blanc on 09/09/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
