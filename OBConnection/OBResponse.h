d1237e665c420621426d79bc42b53c3393c307f9
Oriol Blanc <oriolblanc@gmail.com>
2012-09-09 10:28:27 +0200

    working on response

--- OBConnection/OBResponse.h
+++ OBConnection/OBResponse.h
@@ -6,8 +6,32 @@
 //  Copyright (c) 2012 Oriol Blanc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+typedef enum {
+    OBResponseCodeUndefinedError        = 0,
+    OBResponseCodeNoError               = 200,
+    
+    OBResponseCodeGlobalError           = 110,
+    OBResponseCodeExpiredSession        = 111,
+    OBResponseCodeInvalidParam          = 112,
+    OBResponseCodeAuthenticationError   = 403,
+    
+    
+    OBResponseCodeLogout                = 1000
+} OBResponseCode;
 
 @interface OBResponse : NSObject
+
+@property (nonatomic, assign) NSInteger     statusCode;
+@property (nonatomic, retain) NSString      *message;
+@property (nonatomic, retain) NSDictionary  *body;
+@property (nonatomic, retain) NSDictionary  *headerFields;
+
++ (OBResponse *)responseWithDictionary:(NSDictionary *)response
+                          headerFields:(NSDictionary *)headerFields;
+
++ (OBResponse *)responseWithCode:(NSInteger)statusCode
+                         message:(NSString *)message
+                            body:(NSDictionary *)body
+                    headerFields:(NSDictionary *)headerFields;
 
 @end
--- OBConnection/OBResponse.m
+++ OBConnection/OBResponse.m
@@ -1,12 +1,87 @@
 //
 //  OBResponse.m
-OBConnection//
+//  OBConnection
+//
 //  Created by Oriol Blanc on 20/04/11.
 //  Copyright (c) 2012 Oriol Blanc. All rights reserved.
 //
 
 #import "OBResponse.h"
+@interface OBResponse ()
+- (id)initWithDictionary:(NSDictionary *)response headerFields:(NSDictionary *)headerFields;
+- (id)initWithCode:(NSInteger)statusCode message:(NSString *)message body:(NSDictionary *)body headerFields:(NSDictionary *)headerFields;
+@end
 
 @implementation OBResponse
+@synthesize statusCode = _statusCode;
+@synthesize message = _message;
+@synthesize body = _body;
+@synthesize headerFields = _headerFields;
+
++ (OBResponse *)responseWithDictionary:(NSDictionary *)response
+                          headerFields:(NSDictionary *)headerFields
+{
+    return [[[self alloc] initWithDictionary:response headerFields:headerFields] autorelease];
+}
+
++ (OBResponse *)responseWithCode:(NSInteger)statusCode
+                         message:(NSString *)message
+                            body:(NSDictionary *)body
+                    headerFields:(NSDictionary *)headerFields
+{
+    return [[[self alloc] initWithCode:statusCode message:message body:body headerFields:headerFields] autorelease];
+}
+
+- (id)initWithDictionary:(NSDictionary *)response headerFields:(NSDictionary *)headers
+{
+    if ((self = [super init]))
+    {
+        if (response != nil)
+        {
+            self.statusCode = OBResponseCodeNoError;
+            self.body = response;
+            self.headerFields = headers;
+        }
+        else
+        {
+            self.statusCode = OBResponseCodeGlobalError;
+            self.body = response;
+            self.message = nil;
+        }
+    }
+    
+    return self;
+}
+
+- (id)initWithCode:(NSInteger)code
+           message:(NSString *)responsMessage
+              body:(NSDictionary *)responseBody
+      headerFields:(NSDictionary *)responseHeaders
+{
+    
+    if ((self = [super init]))
+    {
+        if (code == OBResponseCodeUndefinedError)
+            self.statusCode = OBResponseCodeGlobalError;
+        else
+            self.statusCode = code;
+        
+        self.message = responsMessage;
+        self.body = responseBody;
+        self.headerFields = responseHeaders;
+    }
+    
+    return self;
+}
+
+#pragma mark - Memory Management
+
+- (void)dealloc {
+    [_message release];
+    [_body release];
+    [_headerFields release];
+    
+    [super dealloc];
+}
 
 @end
