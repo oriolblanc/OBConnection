9060016c7caa71e309dc56f3a402fea724053d6b
Oriol Blanc <oriolblanc@gmail.com>
2012-09-11 10:21:41 +0200

    first commit for cache

---
+++ OBConnection/OBCache.h
@@ -0,0 +1,13 @@
+//
+//  OBCache.h
+//  OBConnection
+//
+//  Created by Oriol Blanc on 02/12/11.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OBCache : NSObject
+
+@end
---
+++ OBConnection/OBCache.m
@@ -0,0 +1,13 @@
+//
+//  OBCache.m
+//  OBConnection
+//
+//  Created by Oriol Blanc on 02/12/11.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "OBCache.h"
+
+@implementation OBCache
+
+@end
