20c919def07aa5d095c4ce9758014c5a5942248a
Oriol Blanc <oriolblanc@gmail.com>
2012-10-29 22:53:11 +0100

    First commit for the library

---
+++ Framework/OBConnections/OBConnections/OBConnections.h
@@ -0,0 +1,13 @@
+//
+//  OBConnections.h
+//  OBConnections
+//
+//  Created by Oriol Blanc on 29/10/12.
+//  Copyright (c) 2012 OBConnections. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OBConnections : NSObject
+
+@end
---
+++ Framework/OBConnections/OBConnections/OBConnections.m
@@ -0,0 +1,13 @@
+//
+//  OBConnections.m
+//  OBConnections
+//
+//  Created by Oriol Blanc on 29/10/12.
+//  Copyright (c) 2012 OBConnections. All rights reserved.
+//
+
+#import "OBConnections.h"
+
+@implementation OBConnections
+
+@end
