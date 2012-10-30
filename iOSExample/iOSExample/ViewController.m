dd5345502f003a7ba2305abb4ad010b5f82fb279
Oriol Blanc <oriolblanc@gmail.com>
2012-10-30 21:52:05 +0100

    Working on Upload Example

--- iOSExample/iOSExample/ViewController.m
+++ iOSExample/iOSExample/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
     - (IBAction)uploadButtonPressed:(id)sender;
+    - (void)uploadImage:(UIImage *)imageToUpload;
 @end
 
 @implementation ViewController
@@ -23,14 +24,32 @@
     [self presentModalViewController:picker animated:YES];
 }
 
+- (void)uploadImage:(UIImage *)imageToUpload
+{
+    OBRequestParameters *parameters = [OBRequestParameters emptyRequestParameters];
+    [parameters setValue:@"8a7539e238429a6b0138429b3b0a0001" forKey:@"userId"];
+    
+    OBRequestParameters *files = [OBRequestParameters emptyRequestParameters];
+    [files setValue:imageToUpload forKey:@"image"];
+    
+    OBRequest *uploadImageRequest = [OBRequest requestWithType:OBRequestMethodTypeMultiForm resource:@"user/avatar" parameters:parameters files:files isPublic:YES];
+    
+    [OBConnection makeRequest:uploadImageRequest success:^(id data, BOOL cached) {
+        NSLog(@"success");
+    } error:^(id data, NSError *error) {
+        NSLog(@"error");
+    }];
+}
+
 #pragma mark - UIImagePickerController
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
-    // Access the uncropped image from info dictionary
-    UIImage * image = [info objectForKey:@"UIImagePickerControllerEditedImage"];
     [picker dismissModalViewControllerAnimated:YES];
     
+    // Access the uncropped image from info dictionary
+    UIImage * image = [info objectForKey:@"UIImagePickerControllerEditedImage"];
+    [self uploadImage:image];
 }
 
 
